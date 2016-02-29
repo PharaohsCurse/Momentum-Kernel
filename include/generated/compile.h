0b1bc8311cb7b001843a26e75bc6456edba782f6
Joe <joemonyman@gmail.com>
2016-02-28 18:43:55 -0700

    First commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Thu Feb 25 23:15:40 MST 2016"
+#define LINUX_COMPILE_BY "joe"
+#define LINUX_COMPILE_HOST "joe-GA-78LMT-USB3"
+#define LINUX_COMPILER "gcc version 4.7.4 (crosstool-NG linaro-1.13.1+bzr2694 - Linaro GCC 2014.06) "
