ee6ec81dcd3e8ae241cb7042d5a4c8bb0ee215e8
Joe <joemonyman@gmail.com>
2016-03-29 09:20:47 -0600

    MM Kernel update

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Mar 13 14:18:29 MDT 2016"
+#define LINUX_COMPILE_BY "joe"
+#define LINUX_COMPILE_HOST "joe-GA-78LMT-USB3"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.06 - Cortex-A7) "
